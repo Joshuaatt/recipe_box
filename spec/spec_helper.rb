8bed4dbd3c73e5d201825dc0674c154bc4d5acab
Heather Anderson & Joshua Atteberry <student@epicodus.com>
2015-01-29 10:13:46 -0800

    Add working spec to Recipe.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'bundler/setup'
+Bundler.require :default, :test
+
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after :each do
+    Recipe.all.each do |recipe|
+      recipe.destroy
+    end
+    Ingredient.all.each do |ingredient|
+      ingredient.destroy
+    end
+  end
+end
